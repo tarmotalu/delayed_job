b194ea901af70b85022fd8744018731804ffb177
Tarmo Talu <tarmo.talu@wisesoft.ee>
2012-02-26 18:29:11 +0200

    Fix for 'uninitialized constant Syck::Syck'.

--- lib/delayed/syck_ext.rb
+++ lib/delayed/syck_ext.rb
@@ -14,7 +14,7 @@ class Module
   def yaml_tag_read_class(name)
     # Constantize the object so that ActiveSupport can attempt
     # its auto loading magic. Will raise LoadError if not successful.
-    name.constantize
+    name.constantize if name !~ /^YAML/
     name
   end
 end
